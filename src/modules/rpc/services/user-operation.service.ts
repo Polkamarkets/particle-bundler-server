d87037ede5436dc0e38e50d075747b8e843c089c
Ricardo Marques <ricardo.ds.marques@gmail.com>
2024-01-22 01:03:46 +0000

    fix(createOrUpdateUserOperation): allow replacing of txs with over 3600 seconds

--- src/modules/rpc/services/user-operation.service.ts
+++ src/modules/rpc/services/user-operation.service.ts
@@ -41,9 +41,8 @@ export class UserOperationService {
             const transaction = await this.transactionModel.findOne({ chainId, txHash: userOpDoc.txHash });
             // Allowing to replace tx if it happened more than 60 minutes ago
             if (
-                !transaction ||
-                transaction.status !== TRANSACTION_STATUS.FAILED ||
-                Date.now() - transaction.latestSentAt.getTime() > 60 * 60 * 1000
+                Date.now() - userOpDoc.createdAt.getTime() > 60 * 60 * 1000 &&
+                (!transaction || transaction.status !== TRANSACTION_STATUS.FAILED)
             ) {
                 throw new AppException(-32004);
             }
